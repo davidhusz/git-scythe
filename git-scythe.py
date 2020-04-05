f8a404357d2b294cc5818634191ede83f38e9716
David Husz <contact@davidhuszmusic.com>
2020-04-05 02:49:32 +0200

    moved content of config.py back into git-scythe.py and wrapped it in a 'static class' (just like modules)

--- git-scythe.py
+++ git-scythe.py
@@ -8,8 +8,6 @@
 # what about 'TAKE'?
 
 
-import config
-
 import sys
 import os
 import subprocess
@@ -150,6 +148,29 @@ class ScytheParser(argparse.ArgumentParser):
 			args.input = max(files, key = os.path.getatime)
 
 		return args
+
+
+class config:
+	# this class works similarly to the class modules,
+	# see that class's docstring
+	@staticmethod
+	def get(key, default):
+		try:
+			value = subprocess.run(
+				['git', 'config', '--get', 'scythe.' + key],
+				capture_output = True,
+				text = True,
+				check = True
+			).stdout.strip()
+		except subprocess.CalledProcessError:
+			subprocess.run(
+				['git', 'config', '--add', 'scythe.' + key, default],
+				capture_output = True,
+				check = True
+			)
+			value = default
+
+		return value
 
 
 class modules:
